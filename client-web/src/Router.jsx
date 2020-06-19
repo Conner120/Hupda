37a0b8598978febd4bdf8dc86f1a7db1cc64ea66
conner <Conner@reitmeier.org>
2020-06-18 20:00:18 -0500

    apps

---
+++ client-web/src/Router.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {
+    Switch,
+    IndexRoute,
+    Route,
+    Link
+} from "react-router-dom";
+export default function Router() {
+    return (
+        <Route path="/">
+        </Route>
+    );
+}
